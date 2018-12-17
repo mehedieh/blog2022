40b26fd9618176fd41b4a1dd5b98191579dbaf8d
euler16 <nilayshrivastava1729@gmail.com>
2018-12-18 00:12:51 +0530

    completed most of the sections

--- demos/image-captioning/js/caption.js
+++ demos/image-captioning/js/caption.js
@@ -120,15 +120,6 @@ function caption(img) {
     }); 
 }
 
-async function loadMobileNet() {
-    const mobilenet = await tf.loadModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
-    const layer = mobilenet.getLayer('conv_preds');
-    console.log("mobileNet loaded");
-    return tf.model({
-        'inputs': mobilenet.inputs,
-        'outputs': layer.output
-    });
-}
 
 async function start() {
     //mobileNet = loadMobileNet();
